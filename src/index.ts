9398d620f5e8f680fc203f6b77fcea4a68680ee9
michaelsmorgon <smorgon@int.pl>
2023-08-12 20:29:09 +0200

    feat: add registration view

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 import './style.css';
+// import { RegistrationView } from './view/registration/RegistrationView';
+
+// const regView = new RegistrationView();
+
+// const body = document.querySelector('body');
+// body?.appendChild(regView.getHtmlElement());
