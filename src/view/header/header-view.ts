97021dc3a1dcd52a926c2db7278b7bacb3be1f74
AndreWAr85 <andreyarabchyk@gmail.com>
2023-08-13 14:16:57 +0200

    feat: add click handling and active link highlighting in menu

--- src/view/header/header-view.ts
+++ src/view/header/header-view.ts
@@ -61,33 +61,58 @@ function createHeaderContainer(): { headerCenter: ElementCreator; headerUser: El
   return { headerCenter, headerUser };
 }
 
+function handleLinkClick(clickedLink: HTMLElement | null): void {
+  const menuLinks = document.querySelectorAll('.menu a');
+  menuLinks.forEach((link) => link.classList.remove('active'));
+  if (clickedLink) {
+    clickedLink.classList.add('active');
+  }
+}
+
+const clickableClassNames = [
+  'logo-img',
+  'registration-button',
+  'logout-button',
+  'login-button',
+  'basket-logo',
+  'account-icon',
+];
+
 function createMenu(headerCenter: ElementCreator): void {
   const headerMenu = new ElementCreator({
     tag: 'nav',
     classNames: ['menu'],
   });
 
-  const catalogLink = new ElementCreator({
-    tag: 'a',
-    attributes: [{ name: 'href', value: '#catalog' }],
-    classNames: [],
-    textContent: 'Catalog',
+  const menuLinks = [
+    { text: 'Catalog', href: '#catalog' },
+    { text: 'About Us', href: '#about_as' },
+  ];
+
+  menuLinks.forEach((linkInfo) => {
+    const link = new ElementCreator({
+      tag: 'a',
+      attributes: [{ name: 'href', value: linkInfo.href }],
+      classNames: [],
+      textContent: linkInfo.text,
+    });
+
+    link.getElement().addEventListener('click', (event) => {
+      event.preventDefault();
+      handleLinkClick(link.getElement());
+    });
+
+    headerMenu.addInnerElement(link);
   });
 
-  const aboutUsLink = new ElementCreator({
-    tag: 'a',
-    attributes: [
-      {
-        name: 'href',
-        value: '#about_as',
-      },
-    ],
-    classNames: [],
-    textContent: 'About Us',
+  document.addEventListener('click', (event) => {
+    const clickedElement = event.target as HTMLElement;
+
+    if (clickableClassNames.some((className) => clickedElement.classList.contains(className))) {
+      handleLinkClick(null);
+    }
   });
 
-  headerMenu.addInnerElement(catalogLink);
-  headerMenu.addInnerElement(aboutUsLink);
   headerCenter.addInnerElement(headerMenu);
 }
 
