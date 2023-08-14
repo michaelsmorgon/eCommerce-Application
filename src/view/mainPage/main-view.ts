b98172047b2d210528697f4097b42075d0443929
AndreWAr85 <andreyarabchyk@gmail.com>
2023-08-14 16:43:39 +0200

    feat: add benefets section

--- src/view/mainPage/main-view.ts
+++ src/view/mainPage/main-view.ts
@@ -1,7 +1,9 @@
 import ElementCreator from '../../util/ElementCreator';
-import ProductCategories from './produkt-categories';
+import ProductCategories from './category-products/produkt-categories';
 import MainBanner from './main-banner';
-import CategoriesMainSektion from './categories-section';
+import CategoriesMainSektion from './category-products/categories-section';
+import BenefitsSectionCreator from './benefits/benefits-creator';
+import MetodsSectionCreator from './benefits/metods-creator';
 
 export default class Main {
   create(): ElementCreator {
@@ -19,10 +21,13 @@ export default class Main {
     const mainBanner = new MainBanner();
     const productCategories = new ProductCategories().create();
     const categoriesSektion = new CategoriesMainSektion().create();
-
+    const benefitsSectionCreator = new BenefitsSectionCreator().create();
+    const metodsSectionCreator = new MetodsSectionCreator().create();
+    benefitsSectionCreator.addInnerElement(metodsSectionCreator);
     section.addInnerElement(productCategories);
     section.addInnerElement(mainBanner);
     section.addInnerElement(categoriesSektion);
+    section.addInnerElement(benefitsSectionCreator);
 
     main.addInnerElement(section);
     parentElement.appendChild(main.getElement());
