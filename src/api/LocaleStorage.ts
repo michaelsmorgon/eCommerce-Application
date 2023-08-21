89c671c838519be97021ce17269518d0e51e67f9
Dumik121 <nazar121duma@gmail.com>
2023-08-21 16:27:03 +0300

    feat: Header view changes

--- src/api/LocaleStorage.ts
+++ src/api/LocaleStorage.ts
@@ -3,9 +3,15 @@ export class LocaleStorage {
 
   public static saveLocalStorage(name: string, value: string): void {
     localStorage.setItem(name, value);
+
+    const storageEvent = new Event('storage');
+    window.dispatchEvent(storageEvent);
   }
 
   public static clearLocalStorage(name: string): void {
     localStorage.removeItem(name);
+
+    const storageEvent = new Event('storage');
+    window.dispatchEvent(storageEvent);
   }
 }
