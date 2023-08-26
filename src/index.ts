9087f9b1f72172aabf415cb20a6a5c08bec0710e
AndreWAr85 <andreyarabchyk@gmail.com>
2023-08-26 14:47:00 +0200

    feat: add product receipt function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { App } from './router/app';
 import './style.css';
-import Products from './view/display-produkt/displayProdukt';
 
 const app: App = new App();
 app.init();
-// eslint-disable-next-line no-new
-new Products(); // дадау часова
