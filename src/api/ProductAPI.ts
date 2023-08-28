e9414b67acb860a70cbe83e9144a7e2fdabfad82
michaelsmorgon <smorgon@int.pl>
2023-08-28 19:13:16 +0200

    feat: add product api

---
+++ src/api/ProductAPI.ts
@@ -0,0 +1,71 @@
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
+import {
+  ClientResponse,
+  Product,
+  ProductPagedQueryResponse,
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+import { BuilderClient } from './BuilderClient';
+import { TokenCacheStore } from './TokenCacheStore';
+import { MessageView } from '../view/message/MessageView';
+
+export class ProductAPI {
+  private ctpClient;
+
+  private apiRoot: ByProjectKeyRequestBuilder;
+
+  constructor(tokenCacheStore: TokenCacheStore) {
+    const builderClient: BuilderClient = new BuilderClient(tokenCacheStore);
+    this.ctpClient = builderClient.httpMiddleware();
+    this.apiRoot = createApiBuilderFromCtpClient(this.ctpClient).withProjectKey({
+      projectKey: builderClient.PROJECT_KEY,
+    });
+  }
+
+  public async getProducts(): Promise<ClientResponse<ProductPagedQueryResponse | null>> {
+    try {
+      const response = await this.apiRoot.products().get().execute();
+
+      console.log(response);
+      return response;
+    } catch (error: unknown) {
+      this.showError(error);
+      throw new Error();
+    }
+  }
+
+  public async getProductByKey(key: string): Promise<ClientResponse<Product>> {
+    try {
+      const response = await this.apiRoot.products().withKey({ key }).get().execute();
+
+      console.log(response);
+      return response;
+    } catch (error: unknown) {
+      this.showError(error);
+      throw new Error();
+    }
+  }
+
+  public async getProductById(id: string): Promise<ClientResponse<Product>> {
+    try {
+      const response = await this.apiRoot.products().withId({ ID: id }).get().execute();
+
+      console.log(response);
+      return response;
+    } catch (error: unknown) {
+      this.showError(error);
+      throw new Error();
+    }
+  }
+
+  private showError(error: unknown): void {
+    let str = '';
+    if (typeof error === 'string') {
+      str = error;
+    } else if (error instanceof Error) {
+      str = error.message;
+    }
+    const msgView = new MessageView(str, false);
+    msgView.showWindowMsg();
+  }
+}
