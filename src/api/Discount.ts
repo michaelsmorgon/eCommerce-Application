71aa4b80af58572a0e5c22a7f3abbdd3c781f9b1
michaelsmorgon <smorgon@int.pl>
2023-09-17 14:34:00 +0200

    feat: add discount API

---
+++ src/api/Discount.ts
@@ -0,0 +1,25 @@
+import {
+  ClientResponse,
+  ProductDiscountPagedQueryResponse,
+  createApiBuilderFromCtpClient,
+} from '@commercetools/platform-sdk';
+import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
+import { BuilderClient } from './BuilderClient';
+import { TokenCacheStore } from './TokenCacheStore';
+
+export class Discount {
+  constructor(private tokenCacheStore: TokenCacheStore) {}
+
+  public async getCodes(): Promise<ClientResponse<ProductDiscountPagedQueryResponse>> {
+    const builderClient: BuilderClient = new BuilderClient(this.tokenCacheStore);
+    const ctpClient = builderClient.authWithAnonymousSessionFlow();
+    const apiRoot: ByProjectKeyRequestBuilder = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
+      projectKey: builderClient.PROJECT_KEY,
+    });
+    try {
+      return await apiRoot.productDiscounts().get().execute();
+    } catch (error: unknown) {
+      throw new Error();
+    }
+  }
+}
